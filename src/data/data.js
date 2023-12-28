ad3f07a4a043929b39b62677f0215f3cfee1450b
sphnikola <sphnikola@gmail.com>
2023-12-28 09:24:31 +0000

    init commit

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const data = {
+  html: {
+    title: "HTML",
+    icon: "/assets/images/icon-html.svg",
+    color: "hsla(22, 100%, 96%, 1)",
+  },
+  css: {
+    title: "CSS",
+    icon: "/assets/images/icon-css.svg",
+    color: "hsla(151, 87%, 94%, 1)",
+  },
+  javascript: {
+    title: "JavaScript",
+    icon: "/assets/images/icon-js.svg",
+    color: "hsla(223, 100%, 96%, 1)",
+  },
+  accessibility: {
+    title: "Accessibility",
+    icon: "/assets/images/icon-accessibility.svg",
+    color: "hsla(277, 100%, 95%, 1)",
+  },
+};
+
+export { data };
