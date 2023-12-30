5c6dd0678c1f8137dd4b4e65e67b10a5637916b8
sphnikola <sphnikola@gmail.com>
2023-12-30 00:36:57 +0000

    Refactor the ListItems code

--- src/ListItems.jsx
+++ src/ListItems.jsx
@@ -1,70 +1,29 @@
-import html from "./assets/images/icon-html.svg";
-import css from "./assets/images/icon-css.svg";
-import js from "./assets/images/icon-js.svg";
-import acsb from "./assets/images/icon-accessibility.svg";
+import { data } from "./data/data";
 
 export function ListItems() {
+  const listItems = data.map((lit) => (
+    <li key={lit.id} className="mb-5 ">
+      <a
+        className="flex gap-x-3 items-center
+           text-[18px] hover:text-[24px] hover:leading-[24px] hover:duration-500 hover:ease-in-out
+           bg-link-bg  font-normal p-5  rounded-xl md:text-[28px] md:hover:text-[34px] md:leading-[34px]"
+      >
+        <div
+          className="h-[40px] w-[40px] rounded-md flex justify-center items-center"
+          style={{ backgroundColor: lit.color }}
+        >
+          <img src={lit.icon} className="w-[28px] " />
+        </div>
+        <div className="">
+          <h2>{lit.title}</h2>
+        </div>
+      </a>
+    </li>
+  ));
   return (
     <>
       <section>
-        <ul className=" uppercase">
-          <li className="mb-5 ">
-            <a
-              className="flex gap-x-3 items-center
-                   text-[18px] hover:text-[24px] hover:leading-[24px] hover:duration-500 hover:ease-in-out
-                   bg-link-bg  font-normal p-5  rounded-xl md:text-[28px] md:hover:text-[34px] md:leading-[34px]"
-            >
-              <div className="h-[40px] w-[40px] bg-html-bg-color rounded-md flex justify-center items-center">
-                <img src={html} className="w-[28px] " />
-              </div>
-              <div className="">
-                <h2>html</h2>
-              </div>
-            </a>
-          </li>
-          <li className="mb-5 ">
-            <a
-              className="flex gap-x-3 items-center
-                   text-[18px] hover:text-[24px] hover:leading-[24px] hover:duration-500 hover:ease-in-out
-                   bg-link-bg font-normal p-5  rounded-xl md:text-[28px] md:hover:text-[34px] md:leading-[34px]"
-            >
-              <div className="h-[40px] w-[40px] bg-css-bg-color  rounded-md flex justify-center items-center">
-                <img src={css} className="w-[28px]" />
-              </div>
-              <div>
-                <h2>css</h2>
-              </div>
-            </a>
-          </li>
-          <li className="mb-5 ">
-            <a
-              className="flex gap-x-3 items-center
-                   text-[18px] hover:text-[24px] hover:leading-[24px] hover:duration-500 hover:ease-in-out
-                   bg-link-bg font-normal p-5  rounded-xl md:text-[28px] md:hover:text-[34px] md:leading-[34px]"
-            >
-              <div className="h-[40px] w-[40px] bg-js-bg-color rounded-md flex justify-center items-center">
-                <img src={js} className="w-[28px]" />
-              </div>
-              <div>
-                <h2>javascript</h2>
-              </div>
-            </a>
-          </li>
-          <li className="mb-5 ">
-            <a
-              className="flex gap-x-3 items-center
-                   text-[18px] hover:text-[24px] hover:leading-[24px] hover:duration-500 hover:ease-in-out
-                   bg-link-bg  font-normal p-5  rounded-xl md:text-[28px] md:hover:text-[34px] md:leading-[34px]"
-            >
-              <div className="h-[40px] w-[40px] bg-ascb-bg-color rounded-md flex justify-center items-center">
-                <img src={acsb} className="w-[28px]" />
-              </div>
-              <div>
-                <h2>accessibility</h2>
-              </div>
-            </a>
-          </li>
-        </ul>
+        <ul className=" uppercase">{listItems}</ul>
       </section>
     </>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,23 +1,31 @@
-const data = [
+import html from "../assets/images/icon-html.svg";
+import css from "../assets/images/icon-css.svg";
+import js from "../assets/images/icon-js.svg";
+import acsb from "../assets/images/icon-accessibility.svg";
+
+export const data = [
   {
+    id: 0,
     title: "HTML",
-    icon: "/assets/images/icon-html.svg",
+    icon: html,
     color: "hsla(22, 100%, 96%, 1)",
   },
   {
+    id: 1,
     title: "CSS",
-    icon: "/assets/images/icon-css.svg",
+    icon: css,
     color: "hsla(151, 87%, 94%, 1)",
   },
   {
+    id: 2,
     title: "JavaScript",
-    icon: "/assets/images/icon-js.svg",
+    icon: js,
     color: "hsla(223, 100%, 96%, 1)",
   },
   {
+    id: 3,
     title: "Accessibility",
-    icon: "/assets/images/icon-accessibility.svg",
+    icon: acsb,
     color: "hsla(277, 100%, 95%, 1)",
   },
 ];
-export { data };
