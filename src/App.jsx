3aa57741fd23829ea7b22b7213cdb271f8a39f0a
sphnikola <sphnikola@gmail.com>
2023-12-31 15:16:18 +0000

    Move frontpage component into the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Container from "./Container";
-import { FrontPage } from "./Frontpage";
+import { FrontPage } from "./pages/Frontpage";
 
 function App() {
   return (
--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -1,5 +1,5 @@
-import { FrontPageWelcomeText } from "./FrontPageWelcomeText";
-import { ListItems } from "./ListItems";
+import { FrontPageWelcomeText } from "../FrontPageWelcomeText";
+import { ListItems } from "../ListItems";
 
 export function FrontPage() {
   return (
